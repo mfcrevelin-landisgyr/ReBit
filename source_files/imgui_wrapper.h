892be60bc0eedcb74e125c08819dc3557d34951f
Crevelin <Mateus.Crevelin@landisgyr.com>
2024-12-05 12:46:25 -0300

    backup

--- source_files/imgui_wrapper.h
+++ source_files/imgui_wrapper.h
@@ -2,6 +2,7 @@
 #define IMGUI_WRAPPER_H
 
 #include "imgui.h"
+#include <imgui_internal.h>
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
@@ -13,29 +14,27 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include "tinyfiledialogs.h"
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
 
-namespace ImGui
-{
-    class Wrapper
-    {
+namespace ImGui {
+    class Wrapper {
     public:
         Wrapper() {}
         virtual ~Wrapper() { Cleanup(); }
 
-        // Main entry point
-        bool Construct(int width = 1280, int height = 720)
-        {
-            // Set GLFW error callback
+    public:
+        virtual bool Create() { return true; }
+        virtual bool Update() { return true; }
+
+    public:
+        bool Construct(int width = 1280, int height = 720) {
             glfwSetErrorCallback(glfw_error_callback);
             if (!glfwInit())
                 return false;
 
-            // Configure OpenGL and GLFW
             glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
             glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
             window = glfwCreateWindow(width, height, title, nullptr, nullptr);
@@ -43,68 +42,55 @@ namespace ImGui
                 return false;
 
             glfwMakeContextCurrent(window);
-            glfwSwapInterval(1); // Enable vsync
+            glfwSwapInterval(1);
 
-            // Setup Dear ImGui context
             IMGUI_CHECKVERSION();
             ImGui::CreateContext();
             ImGuiIO& io = ImGui::GetIO();
-            io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-            io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-            io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+            io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+            io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+            io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
-            // User-defined settings
             if (!Create())
                 return false;
 
-            // Setup Platform/Renderer backends
             ImGui_ImplGlfw_InitForOpenGL(window, true);
             ImGui_ImplOpenGL3_Init(glsl_version);
 
             return true;
         }
 
-        // Starts the main loop
-        void Start()
-        {
+        void Start() {
             if (!window)
                 return;
 
-            while (!glfwWindowShouldClose(window))
-            {
-                // Poll and handle events
+            bool dock_layout_setup = true;
+            while (!glfwWindowShouldClose(window)) {
                 glfwPollEvents();
-                if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0)
-                {
+                if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0) {
                     ImGui_ImplGlfw_Sleep(10);
                     continue;
                 }
 
-                // Start ImGui frame
                 ImGui_ImplOpenGL3_NewFrame();
                 ImGui_ImplGlfw_NewFrame();
                 ImGui::NewFrame();
 
-                // Mandatory dockspace covering the entire viewport
-                ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport());
+                ImGui::DockSpaceOverViewport(0,ImGui::GetMainViewport());
 
-                // User-defined rendering
                 if (!Update())
                     break;
 
-                // Rendering
                 ImGui::Render();
                 int display_w, display_h;
                 glfwGetFramebufferSize(window, &display_w, &display_h);
                 glViewport(0, 0, display_w, display_h);
-                glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-                glClear(GL_COLOR_BUFFER_BIT); // Optional, can be removed for no clearing
+                // glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+                // glClear(GL_COLOR_BUFFER_BIT);
                 ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-                // Update and render additional platform windows
                 ImGuiIO& io = ImGui::GetIO();
-                if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-                {
+                if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
                     GLFWwindow* backup_current_context = glfwGetCurrentContext();
                     ImGui::UpdatePlatformWindows();
                     ImGui::RenderPlatformWindowsDefault();
@@ -115,21 +101,13 @@ namespace ImGui
             }
         }
 
-        // Overridable methods for user customization
-        virtual bool Create() { return true; } // Setup custom ImGui settings
-        virtual bool Update() { return true; } // Main application logic
-
     private:
-
-        static void glfw_error_callback(int error, const char* description)
-        {
+        static void glfw_error_callback(int error, const char* description) {
             std::cerr << "GLFW Error " << error << ": " << description << std::endl;
         }
 
-        void Cleanup()
-        {
-            if (window)
-            {
+        void Cleanup() {
+            if (window) {
                 ImGui_ImplOpenGL3_Shutdown();
                 ImGui_ImplGlfw_Shutdown();
                 ImGui::DestroyContext();
--- source_files/main.cpp
+++ source_files/main.cpp
@@ -1,143 +1,281 @@
 #include "imgui_wrapper.h"
+#include "tinyfiledialogs.h"
+
 #include <filesystem>
-#include <map>
+#include <fstream>
 #include <vector>
 #include <string>
+#include <string_view>
+#include <cstdint>
+#include <stack>
+#include <map>
 
 namespace fs = std::filesystem;
 
-class CSVEditor : public ImGui::Wrapper
+class ReBit : public ImGui::Wrapper
 {
 public:
-    CSVEditor() {
-        title = "CSV Editor";
+    ReBit() {
+        title = "ReBit";
     }
 
     bool Create() override {
-        // Initialization for the editor
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.Fonts->AddFontFromFileTTF("IBMPlexMono-Regular.ttf", 18.0f);
+
+        std::strncpy(path_buffer, current_directory.string().c_str(), sizeof(path_buffer));
+
         return true;
     }
 
     bool Update() override {
-        ShowProjectPanel();
-        ShowFilePanels();
+        ImGui::ShowDemoWindow();
+        directory_panel();
+        tabs_panel();
         return true;
     }
 
 private:
-    // The current directory path
-    fs::path currentDir;
+    void directory_panel(){
+        ImGui::Begin("Project Panel",nullptr,ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove);// ImGuiWindowFlags_NoMove
 
-    // Map of opened files with their data
-    std::map<std::string, std::map<std::string, std::vector<std::string>>> openFiles;
+            ImGui::BeginGroup();
+                if (ImGui::Button("<"))
+                    GoBack();
+            ImGui::SameLine();
+                if (ImGui::Button("^"))
+                    GoUp();
+            ImGui::SameLine();
+                if (ImGui::Button(">"))
+                    GoForward();
+            ImGui::SameLine();
+                if (ImGui::InputText("##", path_buffer, sizeof(path_buffer), ImGuiInputTextFlags_EnterReturnsTrue))
+                    SetCurrentDirectory(path_buffer);
+            ImGui::SameLine();
+                if (ImGui::Button("Browse"))
+                    BrowseDialog();
+            ImGui::EndGroup();
 
-    // Show the project panel on the left side
-    void ShowProjectPanel() {
-        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-        ImGui::Begin("Project Panel", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+            ImGui::Spacing();
 
-        if (ImGui::Button("Select Directory")) {
-            // Open a file dialog to select a directory
-            const char* path = tinyfd_selectFolderDialog("Select a Directory", currentDir.string().c_str());
-            if (path) {
-                currentDir = fs::path(path);
+            ImGui::BeginChild("ChildL", ImGui::GetContentRegionAvail(), ImGuiChildFlags_None, ImGuiWindowFlags_None);
+                if (fs::is_directory(current_directory))
+                    list_directory_content();
+            ImGui::EndChild();
+        
+        ImGui::End();
+
+    }
+
+    void tabs_panel(){
+        ImGui::Begin("Files Panel",nullptr,ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove);
+            // if (ImGui::BeginTabBar("MyTabBar", ImGuiTabBarFlags_None)) {
+            //     if (ImGui::BeginTabItem("Avocado")) {
+            //         ImGui::Text("This is the Avocado tab!\nblah blah blah blah blah");
+            //         ImGui::EndTabItem();
+            //     }
+            //     if (ImGui::BeginTabItem("Broccoli")) {
+            //         ImGui::Text("This is the Broccoli tab!\nblah blah blah blah blah");
+            //         ImGui::EndTabItem();
+            //     }
+            //     if (ImGui::BeginTabItem("Cucumber")) {
+            //         ImGui::Text("This is the Cucumber tab!\nblah blah blah blah blah");
+            //         ImGui::EndTabItem();
+            //     }
+            //     ImGui::EndTabBar();
+            // }
+
+
+
+            static std::vector<int> active_tabs;
+            static int next_tab_id = 0;
+            if (next_tab_id == 0) // Initialize with some default tabs
+                for (int i = 0; i < 3; i++)
+                    active_tabs.push_back(next_tab_id++);
+
+            // TabItemButton() and Leading/Trailing flags are distinct features which we will demo together.
+            // (It is possible to submit regular tabs with Leading/Trailing flags, or TabItemButton tabs without Leading/Trailing flags...
+            // but they tend to make more sense together)
+            static bool show_leading_button = true;
+            static bool show_trailing_button = true;
+            ImGui::Checkbox("Show Leading TabItemButton()", &show_leading_button);
+            ImGui::Checkbox("Show Trailing TabItemButton()", &show_trailing_button);
+
+            // Expose some other flags which are useful to showcase how they interact with Leading/Trailing tabs
+            static ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_AutoSelectNewTabs | ImGuiTabBarFlags_Reorderable | ImGuiTabBarFlags_FittingPolicyResizeDown;
+            ImGui::CheckboxFlags("ImGuiTabBarFlags_TabListPopupButton", &tab_bar_flags, ImGuiTabBarFlags_TabListPopupButton);
+            if (ImGui::CheckboxFlags("ImGuiTabBarFlags_FittingPolicyResizeDown", &tab_bar_flags, ImGuiTabBarFlags_FittingPolicyResizeDown))
+                tab_bar_flags &= ~(ImGuiTabBarFlags_FittingPolicyMask_ ^ ImGuiTabBarFlags_FittingPolicyResizeDown);
+            if (ImGui::CheckboxFlags("ImGuiTabBarFlags_FittingPolicyScroll", &tab_bar_flags, ImGuiTabBarFlags_FittingPolicyScroll))
+                tab_bar_flags &= ~(ImGuiTabBarFlags_FittingPolicyMask_ ^ ImGuiTabBarFlags_FittingPolicyScroll);
+
+            if (ImGui::BeginTabBar("MyTabBar", tab_bar_flags))
+            {
+                // Demo a Leading TabItemButton(): click the "?" button to open a menu
+                if (show_leading_button)
+                    if (ImGui::TabItemButton("?", ImGuiTabItemFlags_Leading | ImGuiTabItemFlags_NoTooltip))
+                        ImGui::OpenPopup("MyHelpMenu");
+                if (ImGui::BeginPopup("MyHelpMenu"))
+                {
+                    ImGui::Selectable("Hello!");
+                    ImGui::EndPopup();
+                }
+
+                // Demo Trailing Tabs: click the "+" button to add a new tab.
+                // (In your app you may want to use a font icon instead of the "+")
+                // We submit it before the regular tabs, but thanks to the ImGuiTabItemFlags_Trailing flag it will always appear at the end.
+                if (show_trailing_button)
+                    if (ImGui::TabItemButton("+", ImGuiTabItemFlags_Trailing | ImGuiTabItemFlags_NoTooltip))
+                        active_tabs.push_back(next_tab_id++); // Add new tab
+
+                // Submit our regular tabs
+                for (int n = 0; n < active_tabs.size(); )
+                {
+                    bool open = true;
+                    char name[16];
+                    snprintf(name, IM_ARRAYSIZE(name), "%04d", active_tabs[n]);
+                    if (ImGui::BeginTabItem(name, &open, ImGuiTabItemFlags_None))
+                    {
+                        ImGui::Text("This is the %s tab!", name);
+                        ImGui::EndTabItem();
+                    }
+
+                    if (!open)
+                        active_tabs.erase(active_tabs.begin() + n);
+                    else
+                        n++;
+                }
+
+                ImGui::EndTabBar();
             }
-        }
-
-        // List files and directories
-        if (!currentDir.empty()) {
-            ShowDirectoryContents(currentDir);
-        }
-
         ImGui::End();
     }
 
-    // Show contents of a directory in the project panel
-    void ShowDirectoryContents(const fs::path& dir) {
-        for (auto& entry : fs::directory_iterator(dir)) {
+private:
+    void list_directory_content() {
+        std::vector<std::filesystem::path> directories;
+        std::vector<std::filesystem::path> bin_files;
+        std::vector<std::filesystem::path> files;
+
+        IsBinary("",true);
+
+        for (auto& entry : fs::directory_iterator(current_directory)) {
             const auto& path = entry.path();
             bool isDir = fs::is_directory(path);
             bool isBinary = IsBinary(path);
 
-            if (isDir) {
-                if (ImGui::Selectable((path.filename().string() + "/").c_str())) {
-                    currentDir = path;  // Navigate into the directory
+            if (isDir)
+                directories.push_back(path);
+            else if (isBinary)
+                bin_files.push_back(path);
+            else
+                files.push_back(path);
+        }
+
+        ImGui::Separator();
+        
+        if (!directories.empty()){
+            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.00f, 0.71f, 0.25f, 1.0f));
+            for (const auto& dir : directories) {
+                if (ImGui::Selectable((dir.filename().string() + "/").c_str())) {
+                    if (ImGui::IsMouseDoubleClicked(0)) {
+                        backwards_directory_history.push(current_directory);
+                        current_directory = dir;
+                    }
                 }
             }
-            else if (!isBinary) {
-                if (ImGui::Selectable(path.filename().string().c_str())) {
-                    OpenFile(path);  // Open the file when clicked
-                }
-            }
-            else {
-                ImGui::Selectable(path.filename().string().c_str(), false);  // Disabled binary files
-            }
+            ImGui::PopStyleColor();
+            ImGui::Separator();
         }
 
-        // Parent directory navigation
-        if (currentDir != fs::path()) {
-            if (ImGui::Button("Up")) {
-                currentDir = currentDir.parent_path();  // Navigate up one directory
-            }
-        }
-    }
-
-    // Determine if a file is binary based on its content
-    bool IsBinary(const fs::path& path) {
-        try {
-            std::ifstream file(path, std::ios::binary);
-            char ch;
-            while (file.get(ch)) {
-                if (ch == '\0') {  // Null character found, likely binary
-                    return true;
-                }
-            }
-        }
-        catch (...) {
-            return false;  // In case of any error, treat as non-binary
-        }
-        return false;  // Assume non-binary if no null character
-    }
-
-    // Open a file and load its data
-    void OpenFile(const fs::path& filePath) {
-        // Load the CSV data (using a placeholder method for now)
-        auto table = LoadFile(filePath.string());
-        openFiles[filePath.string()] = table;
-
-        // Show the file panel
-        ShowFilePanel(filePath.string(), table);
-    }
-
-    // Show the file panel with editable table
-    void ShowFilePanel(const std::string& filePath, std::map<std::string, std::vector<std::string>>& table) {
-        ImGui::Begin(filePath.c_str(), nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
-
-        // Display the table in a grid format
-        for (auto& [key, values] : table) {
-            ImGui::Text("%s", key.c_str());  // Column header
-            for (size_t i = 0; i < values.size(); ++i) {
-                ImGui::InputText(("Cell " + std::to_string(i)).c_str(), &values[i]);
+        for (const auto& file : files) {
+            if (ImGui::Selectable(file.filename().string().c_str())) {
+                std::cout << "File to open : " << file.filename().string() << std::endl; 
             }
         }
 
-        ImGui::End();
+        for (const auto& file : bin_files)
+            ImGui::Selectable(file.filename().string().c_str(), false, ImGuiSelectableFlags_Disabled);
+
     }
 
-    // Dummy method to load a file, returns a simple table for now
-    std::map<std::string, std::vector<std::string>> LoadFile(const std::string& filePath) {
-        // Simulating loading a file into a table format
-        std::map<std::string, std::vector<std::string>> table;
-        table["Header 1"] = {"Row 1", "Row 2", "Row 3"};
-        table["Header 2"] = {"Value 1", "Value 2", "Value 3"};
-        return table;
+    bool IsBinary(const fs::path& path,bool reset=false) {
+        static bool b = false;
+        if (reset)
+            b = reset;
+        else
+            b = !b;
+        return b;
     }
+
+private:
+    void GoBack() {
+        if (!backwards_directory_history.empty()) {
+            forwards_directory_history.push(current_directory);
+            current_directory = backwards_directory_history.top();
+            backwards_directory_history.pop();
+            std::strncpy(path_buffer, current_directory.string().c_str(), sizeof(path_buffer));
+        }
+    }
+
+    void GoUp() {
+        fs::path parent = current_directory.parent_path();
+        if (parent != current_directory) {
+            backwards_directory_history.push(current_directory);
+            current_directory = parent;
+            std::strncpy(path_buffer, current_directory.string().c_str(), sizeof(path_buffer));
+            while(!forwards_directory_history.empty())
+                forwards_directory_history.pop();
+        }
+    }
+
+    void GoForward() {
+        if (!forwards_directory_history.empty()) {
+            backwards_directory_history.push(current_directory);
+            current_directory = forwards_directory_history.top();
+            forwards_directory_history.pop();
+            std::strncpy(path_buffer, current_directory.string().c_str(), sizeof(path_buffer));
+        }
+    }
+
+    void SetCurrentDirectory(const char* new_path) {
+        fs::path new_dir(new_path);
+
+        if (fs::exists(new_dir) && fs::is_directory(new_dir)) {
+            backwards_directory_history.push(current_directory);
+            current_directory = new_dir;
+            while(!forwards_directory_history.empty())
+                forwards_directory_history.pop();
+        }
+
+        std::strncpy(path_buffer, current_directory.string().c_str(), sizeof(path_buffer));
+    }
+
+    void BrowseDialog() {
+        const char* selected_path = tinyfd_selectFolderDialog(
+            "Select a folder",
+            current_directory.string().c_str()
+        );
+
+        if (selected_path != NULL){
+            backwards_directory_history.push(current_directory);
+            current_directory = selected_path;
+            std::strncpy(path_buffer, selected_path, sizeof(path_buffer));
+            while(!forwards_directory_history.empty())
+                forwards_directory_history.pop();
+        }
+    }
+
+private:
+    char path_buffer[512];
+    fs::path current_directory = "C:\\Users\\crevelim\\Downloads";
+    std::stack<fs::path> backwards_directory_history;
+    std::stack<fs::path> forwards_directory_history;
 };
 
 int main() {
-    CSVEditor app;
-    if (app.Construct()) {
+    ReBit app;
+    if (app.Construct())
         app.Start();
-    }
     return 0;
 }
